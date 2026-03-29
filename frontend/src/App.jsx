74471dd19dde346b5b31fc86f037dd62e52659ad
nobamb <jsh12345644@gmail.com>
2026-03-29 23:55:01 +0900

    design: 파비콘 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import Login from './components/Login'
+import { useEffect } from 'react';
+import Login from './components/Login';
+import { getWebpageImageUrl } from './lib/supabase';
 
 function App() {
+  useEffect(() => {
+    const faviconHref = getWebpageImageUrl('favicon.png');
+    let faviconLink = document.querySelector("link[rel='icon']");
+
+    if (!faviconLink) {
+      faviconLink = document.createElement('link');
+      faviconLink.setAttribute('rel', 'icon');
+      document.head.appendChild(faviconLink);
+    }
+
+    faviconLink.setAttribute('type', 'image/png');
+    faviconLink.setAttribute('href', faviconHref);
+  }, []);
+
   return (
     <>
       <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
